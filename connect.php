c5afeb548e56058c89e8efdd22cbf0f2bc7ca9e8
sheryew <tansheryew@gmail.com>
2022-05-13 17:23:54 +0800

    Adding PHP files (Test)

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php 
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $number = $_POST['number'];
+    
+    $conn = new mysqli('localhost', 'root', "reallyStrongPwd123", "CFFLDB");
+    if($conn -> connect_error) {
+        die('Connection Failed' : '.$conn-> connect_error');
+    } else {
+        $stmt = $conn-> prepare("INSERT INTO Users (`firstName`, `lastName`, `gender`, `email`, `password`, `fav`) 
+        VALUES (?,?,?,?,?,?)");
+        $stmt -> bind_param("ssssss", $firstName, $lastName, $gender, $email, $password, $number);
+        $stmt -> execute();
+        echo "registration Sucessfully...";
+        $stmt -> close();
+        $conn -> close();
+    }
+?>
