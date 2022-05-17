f1094f95d0a98e35ff7c55c49cb3e1dc3e5e4f3c
sheryew <tansheryew@gmail.com>
2022-05-17 18:07:54 +0800

    Trying To Figure Out how to get the metamask address

---
+++ metamask.js
@@ -0,0 +1,28 @@
+function toggleButton() {
+    const loginButton = document.getElementById('connect')
+    if (!window.ethereum) {
+        loginButton.addEventListener('click', () => {
+            window.open('https://metamask.io/download', '_blank')
+        });
+    }
+
+    loginButton.addEventListener('click', () => {
+        loginwithMetaMask();
+    });
+}
+
+async function loginwithMetaMask() {
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+        .catch((e) => {
+            console.error(e.message)
+            return
+        })
+    if (!accounts) {return}
+    window.userWalletAddress = accounts[0]
+    console.log(window.userWalletAddress)
+    return window.userWalletAddress
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    toggleButton();
+});
