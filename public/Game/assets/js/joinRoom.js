8b35609e4a620dacb5d4951a7fc111bbaabb923f
sheryew <tansheryew@gmail.com>
2022-06-24 00:30:32 +0800

    Fixed The Bug Regarding The Multiplayer

--- public/Game/assets/js/joinRoom.js
+++ public/Game/assets/js/joinRoom.js
@@ -15,57 +15,100 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const currentUser = JSON.parse(localStorage.getItem('usr'));
-var userName = ref(db, 'users/' + currentUser.uid + '/lastName');
+var userName = ref(db, 'users/' + currentUser.uid);
+//this is for the creation of game room!
 document.getElementById('btn1').addEventListener('click', () => {
     get(userName).then((snapshot) => {
-        const userLN = snapshot.val();
-        const newPostKey = push(child(ref(db), 'Rooms')).key;
-        alert('Send The Unique Room To Your User: ' + newPostKey);
-            //  -N53RoIIxLrfR3Z2IgBU
-        const updates = {};
-        updates['/Rooms/' + newPostKey] = {
-            owner: currentUser.uid,
-            memberNames: {1: userLN}, 
-            memberCount: 1,
-            messages: '',
+        const data = snapshot.val();
+        const userRoom = data['onlineRoom'];
+        console.log(userRoom)
+        if (userRoom != "") {
+            alert('You Cannot Mulitple Rooms!')
+        } else {
+            console.log('I am Here!')
+            const userLN = data['lastName']
+            const newPostKey = push(child(ref(db), 'Rooms')).key;
+            alert('Send The Unique Room To Your User: ' + newPostKey);
+                //  -N53RoIIxLrfR3Z2IgBU
+            const updates = {};
+            console.log(newPostKey)
+            updates['/Rooms/' + newPostKey] = {
+                owner: currentUser.uid,
+                memberNames: {1: userLN}, 
+                memberCount: 1,
+                messages: '',
+                memberUID: {1: currentUser.uid}
+            }
+            update(ref(db), updates);
+            update(ref(db, 'users/' + currentUser.uid), {
+                'onlineRoom':  newPostKey,
+            })
+            window.location.href = "multiplayerGame.html"
         }
-        update(ref(db), updates);
-        update(ref(db, 'users/' + currentUser.uid), {
-            'onlineRoom':  newPostKey,
-        })
-        window.location.href = "multiplayerGame.html"
     })
 })
 
+//this is for joining of game room!
 document.getElementById('btn2').addEventListener('click', () => {
     var meetingID = document.getElementById('text1').value.toString();
     console.log(meetingID)
     const starCountRef = ref(db);
     get(starCountRef).then((snapshot) => {
         const prevData = snapshot.val();
-        const data = prevData['Rooms'];
-        var arryOfKeys = [];
-        for (var key in data) {
-            arryOfKeys.push(key)
-        }
-        if (arryOfKeys.includes(meetingID)) {
-            const currentMC = data[meetingID]['memberCount'];
-            var newCount = currentMC + 1;
-            const arry = data[meetingID]['memberNames']
-            var userName = prevData['users'][currentUser.uid]['lastName'];
-            arry[newCount] = userName;
-            const updates = {};
-            update(ref(db, 'Rooms/' + meetingID), {
-                'memberCount': newCount,
-                'memberNames': arry
-            })
-            update(ref(db, 'users/' + currentUser.uid), {
-                'onlineRoom':  meetingID,
-            })
-            alert('Done')
-            window.location.href = "multiplayerGame.html"
+        const onlineRoom2 = prevData['users'][currentUser.uid]['onlineRoom'];
+        if (onlineRoom2 != "") {
+            alert('Cannot Join Multiple Rooms!')
         } else {
-            alert('Sorry No Such Room Exists!')
+            const data = prevData['Rooms'];
+            //arryofKeys consist of all the Rooms' meeting ID
+            var arryOfKeys = [];
+            for (var key in data) {
+                arryOfKeys.push(key)
+            }
+            if (arryOfKeys.includes(meetingID)) {
+                const data2 = prevData['Rooms'][meetingID]['memberUID']
+                //arryofUID consists of all the memberUID that belongs in the room
+                var arryofUID = [];
+                for (var key2 in data2) {
+                    arryofUID.push(data2[key2])
+                }
+                if (!arryofUID.includes(currentUser.uid)) {
+                    const starCountRef = ref(db, 'Rooms/' + meetingID + '/memberCount');
+                    //Not too sure what will happen if two fight for the same spots --> got to run some tests here!
+                    get(starCountRef).then((snapshot) => {
+                        var amt = snapshot.val();
+                        if (amt < 5) {
+                            //This is to update memberCount & 
+                            const currentMC = data[meetingID]['memberCount'];
+                            var newCount = currentMC + 1;
+                            const arry = data[meetingID]['memberNames']
+                            var userName = prevData['users'][currentUser.uid]['lastName'];
+                            arry[newCount] = userName;
+                            //this is to update memberUID
+                            const arry2 = data[meetingID]['memberUID']
+                            arry2[newCount] = currentUser.uid;
+                            update(ref(db, 'Rooms/' + meetingID), {
+                                'memberCount': newCount,
+                                'memberNames': arry,
+                                'memberUID': arry2
+                            })
+                            //This is to retrieve the Online Room the User is currently in
+                            update(ref(db, 'users/' + currentUser.uid), {
+                                'onlineRoom':  meetingID,
+                            })
+                            alert('Redirecting!')
+                            window.location.href = "multiplayerGame.html"
+                        } else {
+                            alert('Limit Is Reached')
+                        }
+                    })
+                } else {
+                    //The user already belongs in the room -- so just redirect will do
+                    window.location.href = "multiplayerGame.html"
+                }
+            } else {
+                alert('No Such Rooms Exists!')
+            }
         }
     })
 })
--- public/Game/assets/js/warRoom.js
+++ public/Game/assets/js/warRoom.js
@@ -22,19 +22,35 @@ get(meetingID).then((snapshot) => {
     const starCountRef = ref(db, 'Rooms/' + mid+ '/memberNames');
     onValue(starCountRef, (snapshot) => {
         const data = snapshot.val();
-        var counter = 1;
-        for (var k = 1; k < data.length; k++) {
-            console.log(data[k])
-            if (data[k] == 'undefined') {
-                continue;
+        var arryofI = [];
+        for (var k in data) {
+            arryofI.push(k);
+        }
+        console.log(arryofI)
+        for (var j = 1; j <=5; j++) {
+            if (arryofI.includes(j.toString())) {
+                var numBefore = j.toString() - 1;
+                if (!arryofI.includes(numBefore) || j.toString() == '1') {
+                    document.getElementById(j).innerText = data[j];
+                } else {
+                    document.getElementById(numBefore).innerText = data[j];
+                }
             } else {
-                document.getElementById(counter).innerText = data[k];
-                document.getElementById('numofP').innerText = `${data.length - 1}` + '/5';
-                counter++
+                console.log(2)
+                document.getElementById(j).innerText = "";
             }
         }
     });
 
+
+    //This to reflect the number of participants accurately
+    const starCountRef3 = ref(db, 'Rooms/' + mid+ '/memberCount');
+    onValue(starCountRef3, (snapshot3) => {
+        const data3 = snapshot3.val();
+        document.getElementById('numofP').innerText = `${data3}` + '/5';
+    })
+
+    /*
     //THIS IS TO SEND MESSAGE
     document.getElementById('sendBtn').addEventListener('click', (e) => {
         const user = ref(db, 'users/' + currentUser.uid);
@@ -57,9 +73,38 @@ get(meetingID).then((snapshot) => {
         const userLN = snapshot.val();
         onChildAdded(newMag, (data) => {
             var divData = 
-            '<div class="commenterImage"><i class="fa-solid fa-circle-user" style = "margin-left: 20%"></i></div><div class="commentText"><p class="">' + data.val().message + '</p> <span class="date sub-text">' + 'Sent By ' + data.val().name + '</span></div>'
+            '<li><div class="commenterImage"><i class="fa-solid fa-circle-user" style = "margin-left: 20%"></i></div><div class="commentText"><p class="">' + data.val().message + '</p> <span class="date sub-text">' + 'Sent By ' + data.val().name + '</span></div><li>'
             var d1 = document.getElementById('bodyContent');
             d1.insertAdjacentHTML('beforebegin', divData);
         })
     })
+    */
+    const numofU = ref(db, 'Rooms/' + mid + '/memberCount');
+    onValue(numofU, (snapshot) => {
+        const numofUsers = snapshot.val();
+        const newNumofusers = numofUsers - 1;
+        var test = firebase.database().ref('Rooms/' + mid + '/memberCount')
+        test.onDisconnect().set(newNumofusers);
+    })
+    
+    const updatememberUID = ref(db, 'Rooms/' + mid + '/memberUID');
+    onValue(updatememberUID, (snapshot2) => {
+        const data2 = snapshot2.val();
+        var index;
+        for (var keys in data2) {
+            if (data2[keys] == currentUser.uid) {
+                index = keys;
+            }
+        }
+        console.log(index)
+
+        var test2 = firebase.database().ref('Rooms/' + mid + '/memberUID/' + index)
+        test2.onDisconnect().remove()
+
+        var test3 = firebase.database().ref('Rooms/' + mid + '/memberNames/' + index)
+        test3.onDisconnect().remove()
+    })
+
+    var test4 = firebase.database().ref('users/' + currentUser.uid + '/onlineRoom')
+    test4.onDisconnect().set("")
 })
