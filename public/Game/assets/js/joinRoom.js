1c59dcc406811c4e64580a43e89be0eabe55a3e7
sheryew <tansheryew@gmail.com>
2022-07-09 14:25:35 +0800

    Added the feature where users need to pay to play multiplayer

--- public/Game/assets/js/joinRoom.js
+++ public/Game/assets/js/joinRoom.js
@@ -20,36 +20,64 @@ var userName = ref(db, 'users/' + currentUser.uid);
 document.getElementById('btn1').addEventListener('click', () => {
     get(userName).then((snapshot) => {
         const data = snapshot.val();
-        const userRoom = data['onlineRoom'];
-        console.log(userRoom)
-        if (userRoom != "") {
-            alert('You Cannot Mulitple Rooms!')
-        } else {
-            console.log('I am Here!')
-            const userLN = data['lastName']
-            const newPostKey = push(child(ref(db), 'Rooms')).key;
-            alert('Send The Unique Room To Your User: ' + newPostKey);
-                //  -N53RoIIxLrfR3Z2IgBU
-            const updates = {};
-            console.log(newPostKey)
-            updates['/Rooms/' + newPostKey] = {
-                owner: currentUser.uid,
-                memberNames: {1: userLN}, 
-                memberCount: 1,
-                readymembers: 0,
-                messages: '',
-                memberUID: {1: currentUser.uid},
-                scoreboard: {1: 0},
-                confirmed: 0,
-                ready_arr: {'nil': 0},
-                played: 0,
-                //scoreboard: {},
+        const userWA = data['walletAddress']
+        sendTransaction()
+        async function sendTransaction() {
+            let params = [{
+                "from": userWA,
+                "to": "0x4c73F4214d36ff12eE35438EC18e6BD97759db64",
+                "gas": Number(210000).toString(16),
+                "gasPrice": Number(1500000000).toString(16),
+                "value": Number(50000000000000000).toString(16),
+            }]
+    
+            let txnHash = await window.ethereum.request({method: "eth_sendTransaction", params})
+                .catch((err) => {
+                    console.log(err)
+                })
+    
+            var receipt =  null;
+            while (receipt == null) {
+                receipt = await window.ethereum.request({method: 'eth_getTransactionReceipt', params: [txnHash]});
             }
-            update(ref(db), updates);
-            update(ref(db, 'users/' + currentUser.uid), {
-                'onlineRoom':  newPostKey,
-            })
-            window.location.href = "multiplayerGame.html"
+
+            if (receipt['status'] == "0x1") {
+                const data = snapshot.val();
+                const userRoom = data['onlineRoom'];
+                console.log(userRoom)
+                if (userRoom != "") {
+                    alert('You Cannot Mulitple Rooms!')
+                } else {
+                    console.log('I am Here!')
+                    const userLN = data['lastName']
+                    const newPostKey = push(child(ref(db), 'Rooms')).key;
+                    alert('Send The Unique Room To Your User: ' + newPostKey);
+                        //  -N53RoIIxLrfR3Z2IgBU
+                    const updates = {};
+                    console.log(newPostKey)
+                    updates['/Rooms/' + newPostKey] = {
+                        owner: currentUser.uid,
+                        memberNames: {1: userLN}, 
+                        memberCount: 1,
+                        readymembers: 0,
+                        messages: '',
+                        memberUID: {1: currentUser.uid},
+                        scoreboard: {1: 0},
+                        confirmed: 0,
+                        ready_arr: {'nil': 0},
+                        played: 0,
+                        //scoreboard: {},
+                    }
+                    update(ref(db), updates);
+                    update(ref(db, 'users/' + currentUser.uid), {
+                        'onlineRoom':  newPostKey,
+                    })
+        
+                    window.location.href = "multiplayerGame.html"
+                }
+            } else {
+                alert('Something went wrong!')
+            }
         }
     })
 })
@@ -61,13 +89,79 @@ document.getElementById('btn2').addEventListener('click', () => {
     const starCountRef = ref(db);
     get(starCountRef).then((snapshot) => {
         const prevData = snapshot.val();
+        const totalRooms = prevData['Rooms']
         const onlineRoom2 = prevData['users'][currentUser.uid]['onlineRoom'];
-        const onlineRoomPlayed = prevData[onlineRoom2]['played'];
-        if (onlineRoom2 != "" && onlineRoom2 != meetingID) {
+        const userWA2 = prevData['users'][currentUser.uid]['walletAddress'] 
+        //arryofKeys consist of all the Rooms' meeting ID
+        var arryOfKeys = [];
+        for (var key in totalRooms) {
+            arryOfKeys.push(key)
+        }
+        const data = prevData['Rooms'];
+        async function sendTransaction() {
+            let params = [{
+                "from": userWA2,
+                "to": "0x4c73F4214d36ff12eE35438EC18e6BD97759db64",
+                "gas": Number(210000).toString(16),
+                "gasPrice": Number(1500000000).toString(16),
+                "value": Number(50000000000000000).toString(16),
+            }]
+    
+            let txnHash = await window.ethereum.request({method: "eth_sendTransaction", params})
+                .catch((err) => {
+                    console.log(err)
+                })
+    
+            var receipt =  null;
+            while (receipt == null) {
+                receipt = await window.ethereum.request({method: 'eth_getTransactionReceipt', params: [txnHash]});
+            }
+            if (receipt['status'] == "0x1") {
+                const starCountRef = ref(db, 'Rooms/' + meetingID + '/memberCount');
+                //Not too sure what will happen if two fight for the same spots --> got to run some tests here!
+                get(starCountRef).then((snapshot) => {
+                    var amt = snapshot.val();
+                    if (amt < 5) {
+                        //This is to update memberCount & 
+                        const currentMC = data[meetingID]['memberCount'];
+                        var newCount = currentMC + 1;
+                        const arry = data[meetingID]['memberNames']
+                        var userName = prevData['users'][currentUser.uid]['lastName'];
+                        arry[newCount] = userName;
+                        //this is to update memberUID
+                        const arry2 = data[meetingID]['memberUID']
+                        arry2[newCount] = currentUser.uid;
+                        update(ref(db, 'Rooms/' + meetingID), {
+                            'memberCount': newCount,
+                            'memberNames': arry,
+                            'memberUID': arry2
+                        })
+                        //This is to retrieve the Online Room the User is currently in
+                        update(ref(db, 'users/' + currentUser.uid), {
+                            'onlineRoom':  meetingID,
+                        })
+                        alert('Redirecting!')
+                        window.location.href = "multiplayerGame.html"
+                    } else {
+                        alert('Limit Is Reached')
+                    }
+                })
+            } else {
+                alert('Something is Wrong!')
+            }
+        }
+        if (onlineRoom2 == "" && arryOfKeys.includes(meetingID)) {
+            sendTransaction()
+        } else if (onlineRoom2 != "" && onlineRoom2 != meetingID) {
             alert('Cannot Join Multiple Rooms!')
-        } else if (onlineRoomPlayed == 1) {
-            alert('The race has already been completed!')
+        } else if (onlineRoom2 != "" && onlineRoom2 == meetingID) {
+            window.location.href = "multiplayerGame.html"
+        } else if (!(arryOfKeys.includes(meetingID))) {
+            alert('No such room exists!')
         } else {
+            alert('Race has already completed!')
+        }
+        /*
             const data = prevData['Rooms'];
             //arryofKeys consist of all the Rooms' meeting ID
             var arryOfKeys = [];
@@ -119,5 +213,6 @@ document.getElementById('btn2').addEventListener('click', () => {
                 alert('No Such Rooms Exists!')
             }
         }
+        */
     })
 })
