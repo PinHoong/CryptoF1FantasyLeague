3fae74596c5b03370ad808c6cdbd59e5d56d00db
Sher Yew <tansheryew@gmail.com>
2022-07-30 18:56:49 +0800

    Fixed The Bug Regarding Inability to purchase crypto

---
+++ public/Game/assets/js/CurrencyInitializer.js
@@ -0,0 +1,28 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import {getDatabase, ref, update, get} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTE9LzXJdLd2-zQFCU9HlxevzanZRbQWg",
+    authDomain: "f1cft-493d1.firebaseapp.com",
+    databaseURL: "https://f1cft-493d1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "f1cft-493d1",
+    storageBucket: "f1cft-493d1.appspot.com",
+    messagingSenderId: "414144945872",
+    appId: "1:414144945872:web:4926d78a5a38ae78b908d5",
+    measurementId: "G-WNE15MGBR0"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const currentUser = JSON.parse(localStorage.getItem('usr'));
+const starCountRef2 = ref(db, 'users/' + currentUser.uid + '/currency');
+const price2 = {'b1': 30, 'b2': 40, 'b3': 50};
+get(starCountRef2).then ((snapshot) => {
+    var data = snapshot.val();
+    localStorage.setItem('Amt', JSON.stringify(data));
+    const newAmt = data + price2[JSON.parse(localStorage.getItem('btn'))];
+    console.log(newAmt);
+    update(ref(db, 'users/' + currentUser.uid), {
+        'currency': newAmt,
+    })
+})
