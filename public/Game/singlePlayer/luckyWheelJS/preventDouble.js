084162e858b66f68a2de0d21a2a0af65fd5bb3f1
Sher Yew <tansheryew@gmail.com>
2022-07-30 16:24:24 +0800

    Fixed the code arrangement for LuckyWheel

---
+++ public/Game/singlePlayer/luckyWheelJS/preventDouble.js
@@ -0,0 +1,25 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import {getDatabase, ref, update, get} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTE9LzXJdLd2-zQFCU9HlxevzanZRbQWg",
+  authDomain: "f1cft-493d1.firebaseapp.com",
+  databaseURL: "https://f1cft-493d1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "f1cft-493d1",
+  storageBucket: "f1cft-493d1.appspot.com",
+  messagingSenderId: "414144945872",
+  appId: "1:414144945872:web:4926d78a5a38ae78b908d5",
+  measurementId: "G-WNE15MGBR0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const currentUser = JSON.parse(localStorage.getItem('usr')); 
+const starCountRef = ref(db, 'users/' + currentUser.uid + '/visited');
+get(starCountRef).then((snapshot) => {
+  const data = snapshot.val();
+  if (data == 1) {
+    document.getElementById('book-a-table-btn2').style.pointerEvents = 'none';
+    document.getElementById('Redeem').style.pointerEvents = 'none';
+  }
+})
